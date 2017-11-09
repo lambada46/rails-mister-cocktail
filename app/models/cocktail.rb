4ac48cb05fee12afb3405e1f0d0be6c134fd5204
lucas furtado <lambada46@gmail.com>
2017-11-09 14:28:12 -0200

    validation of cocktails - added presence: true

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,2 +1,7 @@
 class Cocktail < ApplicationRecord
+  has_many :ingredients
+  has_many :doses, dependent: :destroy
+
+  validates :name, presence: true
+  validates :name, uniquiness: true
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,7 @@
 class Ingredient < ApplicationRecord
+  has_many :doses
+
+  validates :name, uniquiness: true
+
+   # TODO: validation: cant delete if used by at least one cocktail
 end
