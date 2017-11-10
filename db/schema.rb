7297dbdf5cd4e7c949c8694d7be7ba5cbe6a10e5
lucas furtado <lambada46@gmail.com>
2017-11-10 15:03:14 -0200

    added photo option to the new-cocktail formulary

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171109173419) do
+ActiveRecord::Schema.define(version: 20171110155214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20171109173419) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "dose_id"
+    t.string   "photo"
     t.index ["dose_id"], name: "index_cocktails_on_dose_id", using: :btree
   end
 
